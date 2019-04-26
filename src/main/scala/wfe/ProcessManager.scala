0e381cf0e41da4aa6dd47326e6e8eb96323796e5
Phong <tt01zulu@msn.com>
2019-04-26 22:24:29 +0200

    extracted xml text into stand alone resource files

--- src/main/scala/wfe/ProcessManager.scala
+++ src/main/scala/wfe/ProcessManager.scala
@@ -14,50 +14,52 @@ import scala.concurrent.duration._
 import scala.io.Source
 
 object ProcessManager {
+  def parseProcess(process: String) = {
+    val reader = new StringReader(process)
+    val factory = XMLInputFactory.newInstance()
+    val streamReader = factory.createXMLStreamReader(reader)
+    val model = Bpmn.readModelFromStream(new ByteArrayInputStream(process.getBytes))
+    val process1 = model.getModelElementsByType(model.getModel.getType(classOf[Process])).asScala.head
+    var cnt = 0
+    process1
+  }
+  object Processes {
+
+    case class CreateProcess(props: Props, name: String)
+
+  }
+
+  class Processes extends Actor with ActorLogging {
+
+    import Processes._
+
+    var currentProcesses = Set.empty[ActorRef]
+
+    override def receive: Receive = {
+      case CreateProcess(p, name) =>
+        val ref = context.actorOf(p, name)
+        currentProcesses += ref
+        context watch ref
+        ref ! ProcessDefActor.StartProcess()
+        println(s"Currently executing ${currentProcesses.size} processes")
+      case Terminated(ref) =>
+        log.info("Actor terminated....")
+        currentProcesses -= ref
+    }
+
+    override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(maxNrOfRetries = 20, withinTimeRange = 1 minute) {
+      case _: ArithmeticException => Resume
+      case _: NullPointerException => Restart
+      case _: IllegalArgumentException => Stop
+      case _: Exception => Escalate
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     implicit val timeout = Timeout(1.seconds)
 
-    def parseProcess(process: String) = {
-      val reader = new StringReader(process)
-      val factory = XMLInputFactory.newInstance()
-      val streamReader = factory.createXMLStreamReader(reader)
-      val model = Bpmn.readModelFromStream(new ByteArrayInputStream(process.getBytes))
-      val process1 = model.getModelElementsByType(model.getModel.getType(classOf[Process])).asScala.head
-      var cnt = 0
-      process1
-    }
 
-    object Processes {
 
-      case class CreateProcess(props: Props, name: String)
-
-    }
-
-    class Processes extends Actor with ActorLogging {
-
-      import Processes._
-
-      var currentProcesses = Set.empty[ActorRef]
-
-      override def receive: Receive = {
-        case CreateProcess(p, name) =>
-          val ref = context.actorOf(p, name)
-          currentProcesses += ref
-          context watch ref
-          ref ! ProcessDefActor.StartProcess()
-          println(s"Currently executing ${currentProcesses.size} processes")
-        case Terminated(ref) =>
-          log.info("Actor terminated....")
-          currentProcesses -= ref
-      }
-
-      override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(maxNrOfRetries = 20, withinTimeRange = 1 minute) {
-        case _: ArithmeticException => Resume
-        case _: NullPointerException => Restart
-        case _: IllegalArgumentException => Stop
-        case _: Exception => Escalate
-      }
-    }
 
     val parallelJoin = parseProcess(Source.fromResource("parallelJoin.xml").mkString)
     val parallelWithIntermediate = parseProcess(Source.fromResource("parallelWithIntermediate.xml").mkString)
@@ -68,7 +70,7 @@ object ProcessManager {
     val system = ActorSystem("bpmn")
     val processManager = system.actorOf(Props(classOf[Processes]), "processmanager") // /user/processmanager
     processManager ! Processes.CreateProcess(Props(classOf[ProcessDefActor], parallelJoin), "process1")
-        processManager ! Processes.CreateProcess(Props(classOf[ProcessDefActor], parallelWithIntermediate), "process2")
+//        processManager ! Processes.CreateProcess(Props(classOf[ProcessDefActor], parallelWithIntermediate), "process2")
     //    processManager ! Processes.CreateProcess(Props(classOf[ProcessDefActor], rekursionTest), "process3")
     //    processManager ! Processes.CreateProcess(Props(classOf[ProcessDefActor], newtest), "process4")
 
